1bc4fafab94e41faca7870336dc652cbf648236e
StefanoMorice21 <smoricev@ucenfotec.ac.cr>
2026-07-07 20:06:36 -0600

    Creacion Clase Turbina con sus respectivos metodos y Creacion enum EstadoTurbina

---
+++ Entities-DTOs/EstadoTurbina.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities_DTOs
+{
+    public enum EstadoTurbina
+    {
+        ACTIVA,
+        EN_MANTENIMIENTO,
+        DAÑADA,
+        DADA_DE_BAJA
+    }
+}
---
+++ Entities-DTOs/Turbina.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities_DTOs
+{
+    public class Turbina : BaseDTO
+    {
+        public string Nombre { get; set; }
+        public string Ubicacion { get; set; }
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public int anioFabricacion { get; set; }
+        public decimal capacidadKwh { get; set; }
+        public EstadoTurbina Estado { get; set; }
+
+        //Metodos
+
+        public decimal CalcularEnergia(int hora)
+        {
+            return capacidadKwh * hora;
+        }
+        public void CambiarEstado(EstadoTurbina nuevoEstado)
+        {
+            Estado = nuevoEstado;
+        }
+
+    }
+}
