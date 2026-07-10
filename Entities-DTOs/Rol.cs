b18067d5c59ab98a432be2c0b711b65003ad576d
Ronny <ralfarom@ucenfotec.ac.cr>
2026-07-09 21:04:32 -0600

    Creacion clases Rol y User

---
+++ Entities-DTOs/Rol.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities_DTOs
+{
+    public class Rol 
+    {
+        public int Id { get; set; }
+
+        public string NombreRol { get; set; }
+    }
+}
---
+++ Entities-DTOs/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities_DTOs
+{
+    public class User : BaseDTO
+    {
+        public int Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public DateOnly FechaNacimiento { get; set; }
+        public string FotoPerfil { get; set; }
+        public string Contrasena { get; set; }
+        public Rol Rol { get; set; }
+    }
+}
