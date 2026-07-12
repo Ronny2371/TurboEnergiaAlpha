84774b2e2b58222ceb69fd2c654cb6bb349b9867
StefanoMorice21 <smoricev@ucenfotec.ac.cr>
2026-07-11 18:36:56 -0600

    Se anade capa CoreApp y el manager de turbina

---
+++ CoreApp_/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreApp_
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ CoreApp_/TurbinaManager.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess.CRUD;
+using Entities_DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreApp_
+{
+    public class TurbinaManager
+    {
+        public List<Turbina> RetrieveAllTurbinas()
+        {
+            var tCrud = new TurbinaCrudFactory();
+            return tCrud.RetrieveAll<Turbina>();
+        }
+
+        public Turbina RetrieveTurbinaById(int id)
+        {
+            var tCrud = new TurbinaCrudFactory();
+            return tCrud.RetrieveById<Turbina>(id);
+        }
+
+        public void CreateTurbina(Turbina t)
+        {
+            var tCrud = new TurbinaCrudFactory();
+            tCrud.Create(t);
+        }
+        public void UpdateTurbina(Turbina t)
+        {
+            var tCrud = new TurbinaCrudFactory();
+            tCrud.Update(t);
+        }
+        public void DeleteTurbina(Turbina t)
+        {
+            var uCrud = new TurbinaCrudFactory();
+            uCrud.Delete(t);
+        }
+    }
+}
