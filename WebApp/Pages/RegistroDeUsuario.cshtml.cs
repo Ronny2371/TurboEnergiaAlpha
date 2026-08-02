f4a854d65a06fe489203e9b4cd807d19c4aca348
Pablo <pablosociales2020@hotmail.com>
2026-08-01 19:31:29 -0600

    - Se agregó checklist de requisitos de contraseña que se actualiza en tiempo real - Se agregó campo de confirmar contraseña

--- WebApp/Pages/RegistroDeUsuario.cshtml.cs
+++ WebApp/Pages/RegistroDeUsuario.cshtml.cs
@@ -11,8 +11,6 @@ namespace WebApp.Pages
 
         public IActionResult OnPost()
         {
-            // Aquí va la lógica para registrar el usuario
-            // Validar datos, guardar en base de datos, etc.
             return RedirectToPage("/Login");
         }
     }
